7aee0947a44ab4ea7d95453a1b1e5e86be063d02
mjohnson <mjohnson@OKC-L-8CRZTF2>
2021-07-02 10:19:59 -0500

    Build production

---
+++ build/precache-manifest.ad4c2ca1646f45b7e59eafc2869a44df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399d8cdf64bdc7c76e5b4e04ef93c2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae08019fed719d56ac45",
+    "url": "/static/css/2.ae944e3d.chunk.css"
+  },
+  {
+    "revision": "38f8551cb2c2ac08716b",
+    "url": "/static/css/main.e0883a1c.chunk.css"
+  },
+  {
+    "revision": "ae08019fed719d56ac45",
+    "url": "/static/js/2.a194b123.chunk.js"
+  },
+  {
+    "revision": "34dd7979b2801926532dccd7d75e0e9b",
+    "url": "/static/js/2.a194b123.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38f8551cb2c2ac08716b",
+    "url": "/static/js/main.95b8f97f.chunk.js"
+  },
+  {
+    "revision": "8c1142913b7ac2e0fd88",
+    "url": "/static/js/runtime-main.bd18d78b.js"
+  },
+  {
+    "revision": "c02bd4240f2c93b9c298641064f6fdfb",
+    "url": "/static/media/banner.c02bd424.jpg"
+  },
+  {
+    "revision": "8efd579e3a9f974e56c5f6ad3c2acdc2",
+    "url": "/static/media/beach.8efd579e.jpg"
+  },
+  {
+    "revision": "4cfdcd785a816248944454a4dcfb33e2",
+    "url": "/static/media/cat_bouq_07.4cfdcd78.png"
+  },
+  {
+    "revision": "d433776d3f36d8ba83411cf3eb3042e0",
+    "url": "/static/media/corgi.d433776d.jpg"
+  },
+  {
+    "revision": "b13529d5335fbefe56ffd344d740b842",
+    "url": "/static/media/fall.b13529d5.jpg"
+  },
+  {
+    "revision": "d2534bb11d9459302c64548db3e42bab",
+    "url": "/static/media/flower.d2534bb1.jpg"
+  },
+  {
+    "revision": "e9a9ce028304c8e54bf1c1e58abc952e",
+    "url": "/static/media/flower.e9a9ce02.jpg"
+  },
+  {
+    "revision": "9f47a03d4c050e49cb625f1702683c2e",
+    "url": "/static/media/product.9f47a03d.jpg"
+  },
+  {
+    "revision": "ff5c99a4abb36112cc6fbeed731a0401",
+    "url": "/static/media/tree.ff5c99a4.jpg"
+  }
+]);
